c8652e1d9f85fe7bea2bd59d12b1285159a8d343
J Won <jwon117@aucklanduni.ac.nz>
2017-12-03 19:05:16 +1300

    Commit 2: Functioning registration page with POST method.

--- routes/registerDetails.js
+++ routes/registerDetails.js
@@ -1,5 +1,10 @@
 var express = require('express');
+
+var bodyParser = require('body-parser');
 var router = express.Router();
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false}));
 
 // getting the MySQL functionality
 var mysql = require('mysql');
@@ -12,33 +17,60 @@ var con = mysql.createConnection({
     database: "tutorme",
 });
 
-router.get('/', function(req, res) {
+con.connect(function(err){
+    if (err) throw err;
+    console.log("Connected and online!");
+});
 
-    var un = req.query.uname;
-    var pwd = req.query.pw;
-    var fn = req.params.fname;
-    var ln = req.params.lname;
-    var em = req.params.email;
-    var ad = req.params.address;
-    var doob = req.params.dob;
-    var studentID = req.params.sid;
-    var studySchool = req.params.school;
-    var tutor_activation = req.params.tactivation;
+router.post('/', function(req, res) {
 
-    console.log(un);
-    console.log(pwd);
-    console.log(fn);
+    console.log("Working");
 
-    con.connect(function(err){
+    var un = req.body.userName;
+    var pwd = req.body.pwd;
+    var fn = req.body.firstName;
+    var ln = req.body.lastName;
+    var em = req.body.email;
+    var ad = req.body.address;
+    var doob = req.body.dateOfBirth;
+    var studentID = req.body.studentId;
+    var studySchool = req.body.school;
+    var tutor_activation = req.body.tutorActivation;
+
+    console.log("ready to query");
+
+    con.query("INSERT INTO tableUser (username, password, firstName, lastName, email, address, dob, studentID, studySchool, tutor_activation) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [un, pwd, fn, ln, em, ad, doob, studentID, studySchool, tutor_activation], function(err, result) {
+        con.end();
         if (err) throw err;
-        console.log("Connected!");
-
-        con.query("INSERT INTO tableUser (username, password, firstName, lastName, email, address, dob, studentID, studySchool, tutor_activation) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [un, pwd, fn, ln, em, ad, doob, studentID, studySchool, tutor_activation], function(err, result) {
-            if (err) throw err;
-        });
     });
 
-    res.render("registerDetails");
+    res.render("registeredPage", req.body);
+
 });
+
+
+
+// router.get('/', function(req, res) {
+//
+//     var un = req.query.userName;
+//     var pwd = req.query.pwd;
+//     var fn = req.query.firstName;
+//     var ln = req.query.lastName;
+//     var em = req.query.email;
+//     var ad = req.query.address;
+//     var doob = req.query.dateOfBirth;
+//     var studentID = req.query.studentId;
+//     var studySchool = req.query.school;
+//     var tutor_activation = req.query.tutorActivation;
+//
+//     console.log("ready to query");
+//
+//     con.query("INSERT INTO tableUser (username, password, firstName, lastName, email, address, dob, studentID, studySchool, tutor_activation) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [un, pwd, fn, ln, em, ad, doob, studentID, studySchool, tutor_activation], function(err, result) {
+//         con.end();
+//         if (err) throw err;
+//     });
+//
+//     res.render("registeredPage");
+// });
 
 module.exports = router;
