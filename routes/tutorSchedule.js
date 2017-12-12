b81675bf5c7365db7c6e4b49b790e22cba15ef1f
Vanienguyen <vanienguyen2412@gmail.com>
2017-12-12 14:22:17 +1300

    "deal with repeats within the tutorSchedule.js and tutorSchedule.ejs fixed the course string issue" hehe

--- routes/tutorSchedule.js
+++ routes/tutorSchedule.js
@@ -6,59 +6,73 @@ const tutor = {
     "idAvailable": [],
     "idBooked": [],
     "course": [],
-    "tutee": []
+    "tutee": [],
+    "location": []
 }
 
 const availability = {
-    "time": ["12040800", "12041200", "12041300", "12042100", "12040900", "12041100"],
-    "date": [2,3,5,6,7,4]
+    "time": ["12040800", "12041200", "12041300", "12041200", "12042000", "12041000"],
+    "date": [2, 3, 5, 6, 7, 4]
 
 }
 const booking = {
-    "course": ["eat", "sleep", "run"],
-    "tutee": ["lamlam", "pikachu", "jojo"],
+    "course": ["Eat", "Sleep", "Run"],
+    "tutee": ["Lamlam", "Pikachu", "Jojo"],
     "time": ["12040800", "12041200", "12041300"],
-    "date": [2,3,5]
+    "date": [2, 3, 5],
+    "location": ["OGGB", "Science Building", "Clock Tower"]
 }
 
-router.get('/', function(req, res) {
-
+router.get('/', function (req, res) {
 
     const numOfATimeUnit = availability.time;
 
     for (i = 0; i < numOfATimeUnit.length; i++) {
         const id1 = availability.date[i];
-        var id2 = availability.time[i].slice(4,6);
-        if(id2.charAt(0) === "0") {
-            id2 = availability.time[i].slice(5,6);
+        var id2 = availability.time[i].slice(4, 6);
+        if (id2.charAt(0) === "0") {
+            id2 = availability.time[i].slice(5, 6);
         } else {
-            id2 = availability.time[i].slice(4,6);
+            id2 = availability.time[i].slice(4, 6);
         }
-        tutor.idAvailable.push(id1+"."+id2);
+        // tutor.idAvailable.add(id1+"."+id2);
+        num = id1 + "." + id2;
+        if (num.endsWith(0)) {
+            console.log(num);
+            num *= 100;
+        }
+
+        tutor.idAvailable.push(num);
     }
 
     const numOfBTimeUnit = booking.time;
     for (i = 0; i < numOfBTimeUnit.length; i++) {
         const id1 = booking.date[i];
-        var id2 = booking.time[i].slice(4,6);
-        if(id2.charAt(0) === "0") {
-            id2 = booking.time[i].slice(5,6);
+        var id2 = booking.time[i].slice(4, 6);
+        if (id2.charAt(0) === "0") {
+            id2 = booking.time[i].slice(5, 6);
         } else {
-            id2 = booking.time[i].slice(4,6);
+            id2 = booking.time[i].slice(4, 6);
         }
-        tutor.idBooked.push(id1+"."+id2);
+        tutor.idBooked.push(id1 + "." + id2);
         tutor.course.push(booking.course[i]);
+        tutor.tutee.push(booking.tutee[i]);
+        tutor.location.push(booking.location[i]);
     }
-
-    console.log(tutor.course);
-
-
-
-
+    tutor.course = setParsing(tutor.course);
+    tutor.idAvailable = setParsing(tutor.idAvailable);
+    tutor.idBooked = setParsing(tutor.idBooked);
+    tutor.tutee = setParsing(tutor.tutee);
+    tutor.location = setParsing(tutor.location)
 
     res.render('tutorSchedule', tutor);
 
 
 });
+
+function setParsing(list) {
+    var list = new Set(list);
+    return Array.from(list);
+}
 
 module.exports = router;
