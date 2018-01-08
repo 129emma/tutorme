0684e69dfb7e24aae031a6abed73824772f07028
Vanienguyen <vanienguyen2412@gmail.com>
2018-01-09 11:35:05 +1300

    nav bar Tutor schedule hiding df sccs

--- routes/tutor.js
+++ routes/tutor.js
@@ -5,9 +5,10 @@ const router = express.Router();
 
 const home = require('./tutorRoute/home');
 const tutorSchedule = require('./tutorRoute/tutorSchedule');
-const calendar = require('./tutorRoute/calendar');
+const calendar = require('./tutorRoute/tutorBooking');
 const feedback = require('./tutorRoute/feedback');
 const profile = require('./tutorRoute/profile');
+const tutorBooking = require('./tutorRoute/tutorBooking');
 
 
 router.use('/home', home);
@@ -15,5 +16,6 @@ router.use('/tutorSchedule', tutorSchedule);
 router.use('/calendar', calendar);
 router.use('/feedback', feedback);
 router.use('/profile', profile);
+router.use('/tutorBooking', tutorBooking);
 
 module.exports = router;
